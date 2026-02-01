afcebaa2d015e29a155a21cc20ab56bc74f3edbd
rikkimann1 <rmann4@student.touro.edu>
2026-02-01 10:52:57 -0500

    finished the assignment

--- exercises/01_loops.py
+++ exercises/01_loops.py
@@ -6,3 +6,35 @@ TODO:
 4. Print the multiplication table of 5
 
 """
+"""
+1.
+"""
+for i in range(1, 11):
+    print(i)
+
+print("\n")
+"""
+2.
+"""
+for j in range(0, 22, 2):
+    print(j)
+
+print("\n")
+"""
+3.
+"""
+count=0
+for i in range(1, 101):
+    count += i
+
+print(count)
+print("\n")
+"""
+4.
+"""
+
+count=0;
+for n in range (1,13):
+    count= 5*n
+
+    print(count)
--- exercises/04_dictionaries.py
+++ exercises/04_dictionaries.py
@@ -2,3 +2,11 @@
 TODO:
 Create dictionary for a book
 """
+dictionary = {
+    "Author": "A person who writes a book",
+    "Novel": "A long narrative typically in prose form",
+    "Fiction": "Writing that is not based on real events",
+    "Biography": "A detailed account of a person's life",
+    "Audiobook": "A recorded version of a book being read aloud",
+}
+
